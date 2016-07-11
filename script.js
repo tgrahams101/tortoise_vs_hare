540f65fa77888ec5efeabfd8c6fd918e23568791
Ted Anyansi <tanyansi@gmail.com>
2016-07-11 01:37:45 -0700

    YESSS, finally got boththe racing pieces to be able to move simultaneously in response to player clicks

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
 
 $(document).on('ready', function(){
- var i=0;
- var b=0;
+ var x=0;
+ var y=0;
 function resetAll(){
-  $("td").removeClass("makeblack");
-
-   b=0;
-   i=0;
-   d=1;
+  location.reload();
+  // $("td").removeClass("makeblack");
+  //
+  //  b=0;
+  //  i=0;
+  //  d=1;
 }
 
 $("button").on('click', function(){
@@ -20,9 +21,10 @@ $('body').on("keyup", function(event){
     // $("#playerastart").addClass("makeblack");
    if (event.which==87){
 
-   $("#playerastart").addClass("makeblack");
-  i=1;
-   doIStartGame();
+   $("#playerastart").addClass("hare");
+  a=1;
+  startPlayerA();
+  //  doIStartGame();
 }
 });
 //manipulate DOM elements using jquery, checking what you have/changed...need any help..youll do well
@@ -31,47 +33,83 @@ $('body').on("keyup", function(){
   var key=event.which;
 //what about putting a variable that toggles between 0 and 1?
   if (event.which==38){
-  $("#playerbstart").addClass("makeblack");
-  b=1;
-  doIStartGame();
+  $("#playerbstart").addClass("turtle");
+  b=81;
+  // doIStartGame();
+  startPlayerB();
 }
 });
 
- function doIStartGame(){
-    if($("#playerastart").hasClass("makeblack") && $("#playerbstart").hasClass("makeblack")){
-    startGame();
-  }
-}
+//  function doIStartGame(){
+//     if($("#playerastart").hasClass("hare") && $("#playerbstart").hasClass("turtle")){
+//     startPlayerA();
+//     startPlayerB();
+//   }
+// }
 
- function startGame(){
-    if (b==1 && i==1){
-    // for (i=1; i<100; i++){
-    var c=1;
+ function startPlayerA(){
+
     $("body").on("keyup", function(){
 
-        if ($('$book4').hasClass("makeblack")){
-          i=0; b=0; c=0;
-        }
-        else{
+        if ($('#playerastart').hasClass("hare")){
+          if (event.which==87){
+
         //  for(i=1;i<1000; i++){
         // do like a while statement that stops at last row or a
         // if statement that is activated when last row is classed
         // way to construct if statement is if $("#lastrowplayer a").hasClass("makeblack")
+              $('#playerastart').removeClass("hare");
+                $('.book'+a+'').addClass("hare");
+                $('.book'+a-1 +'').removeClass("hare");
+                x=1;
+                a++;
+          }
+        }
+        else if (x==1){
+              if (event.which==87){
+                  var num= a-1;
+                  $('.book'+a+'').addClass("hare");
+                  $('.book'+num +'').removeClass("hare");
+                  a++;
+                }
 
-        var key=event.which;
-        if (event.which==38){
-
-        $('.book'+c+'').addClass("makeblack");
-        c++;
-      }
-
-     // this i counter iterates through column to identify cells
-      }
+        }
       });
     }
+
+        function startPlayerB(){
+          $("body").on("keyup", function(){
+
+        if ($('#playerbstart').hasClass("turtle")){
+          if (event.which==38){
+
+        //  for(i=1;i<1000; i++){
+        // do like a while statement that stops at last row or a
+        // if statement that is activated when last row is classed
+        // way to construct if statement is if $("#lastrowplayer a").hasClass("makeblack")
+              $('#playerbstart').removeClass("turtle");
+                $('.book'+b+'').addClass("turtle");
+                $('.book'+b-1 +'').removeClass("turtle");
+                y=1;
+                b++;
+          }
+        }
+        else if (y==1){
+              if (event.which==38){
+                  var ber= b-1;
+                  $('.book'+b+'').addClass("turtle");
+                  $('.book'+ber +'').removeClass("turtle");
+                  b++;
+                }
+
+        }
+
+      });
+    }
+
     //  }
 
-  }
+
 
 $('#changecourse').on("click",function(){
 $('.container').toggleClass("forest");
